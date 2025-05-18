d914f230ba56080d8ce2200fb86ca296130d3f61
Admin <Admin@LAPTOP-4HRM131V>
2025-05-18 14:10:29 +0300

    Complete

--- HebrewBooksLib/HebrewBooksView.xaml.cs
+++ HebrewBooksLib/HebrewBooksView.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using WpfLib.Helpers;
+using Microsoft.Web.WebView2.Wpf;
 
 namespace HebrewBooksLib
 {
@@ -23,6 +20,100 @@ namespace HebrewBooksLib
         public HebrewBooksView()
         {
             InitializeComponent();
+            LoadRecentBooks();
+            //var vstoDoc = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
+            //vstoDoc.CloseEvent += () =>  SaveRecentBooks();
+        }
+
+        private void SaveRecentBooks()
+        {
+            var recentBooks = new List<string>();
+            foreach (TabItem tabItem in tabControl.Items)
+            {
+                if (tabItem.Tag is HebrewBooksModel hebrewBooksModel)
+                    recentBooks.Add(hebrewBooksModel.ID_Book);
+            }
+
+            string json = JsonSerializer.Serialize(recentBooks);
+            Interaction.SaveSetting(AppDomain.CurrentDomain.FriendlyName, "HebrewBooks", "Recent", json);
+        }
+
+        void LoadRecentBooks()
+        {
+            try
+            {
+                string json = Interaction.GetSetting(AppDomain.CurrentDomain.FriendlyName, "HebrewBooks", "Recent");
+                if (string.IsNullOrEmpty(json))
+                    return;
+
+                var recentBooks = JsonSerializer.Deserialize<List<string>>(json);
+                if (recentBooks == null || recentBooks.Count == 0)
+                    return;
+
+                foreach (string id in recentBooks)
+                {
+                    var item = HebrewBooksManager.BookEntries.FirstOrDefault(b => b.ID_Book == id);
+                    OpenSelectedBook(item);
+                }
+            }
+            catch { }
+        }
+
+        void OpenSelectedBook(HebrewBooksModel entry)
+        {
+            var webView = new WebView2();
+            tabControl.Items.Add(new TabItem { Header = entry.Title, Content = webView, IsSelected = true, Tag = entry });
+
+            LoadBook(webView, entry.ID_Book);
+            UpdatePopularity(entry);
+        }
+
+        async void UpdatePopularity(HebrewBooksModel entry)
+        {
+            entry.Popularity++;
+            if (entry.Popularity > short.MaxValue)
+                await HebrewBooksManager.NormalizePopularityScore();
+            await HebrewBooksManager.SaveBookEntriesListAsync();
+        }
+
+        async void LoadBook(WebView2 webview, string id)
+        {
+            try
+            {
+                webview.Source = new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "LoadingAnimation.html"));
+                string url = $"https://download.hebrewbooks.org/downloadhandler.ashx?req={id}";
+                string fileName = $"{id}.pdf";
+                string downloadPath = Path.Combine(Path.GetTempPath(), fileName);
+
+                if (!File.Exists(downloadPath))
+                {
+                    var handler = new HttpClientHandler { UseCookies = true };
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 ...");
+                        client.DefaultRequestHeaders.Add("Referer", "https://www.hebrewbooks.org/");
+
+                        byte[] fileBytes = await client.GetByteArrayAsync(url);
+                        File.WriteAllBytes(downloadPath, fileBytes);
+                    }
+                }
+
+                if (File.Exists(downloadPath))
+                    webview.Source = new Uri(downloadPath);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+            SaveRecentBooks();
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListBox listBox && listBox.SelectedItem is HebrewBooksModel hebrewBooksModel)
+            {
+                OpenSelectedBook(hebrewBooksModel);
+                listBox.SelectedIndex = -1;
+            }
         }
     }
 }
